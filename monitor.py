380c24cf403f106dbf23f0a4339bc5d0166e0149
rafa <rafammpp@gmail.com>
2018-04-05 18:34:01 +0200

    command line argument for single server scan

--- monitor.py
+++ monitor.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 import requests
-from utils import check_server, recheck_servers, default_ports, abs_path
+from utils import check_server, recheck_servers
 import os
+import sys
+try:
+    from user_settings import default_ports, server_list_path
+except ImportError:
+    from default_settings import default_ports, server_list_path
 
-file = open(os.path.join(abs_path, 'server-list.dat'))
+file = open(server_list_path)
 
 def get_port_list(line):
     if ':' in line and line.split(':')[1]:
@@ -18,14 +23,19 @@ def get_remote_server(line):
     else:
         return line
 
-recheck_servers()
+if len(sys.argv) > 1:
+    s = get_remote_server(sys.argv[1])
+    p = get_port_list(sys.argv[1])
+    check_server(s, p)
+else:
+    recheck_servers()
 
-for line in file:
-    line = line.strip()
-    if line and line[0] != '#':
-        remote_server = get_remote_server(line)
-        port_list = get_port_list(line)
-        check_server(remote_server, port_list)
-file.close()
+    for line in file:
+        line = line.strip()
+        if line and line[0] != '#':
+            remote_server = get_remote_server(line)
+            port_list = get_port_list(line)
+            check_server(remote_server, port_list)
+    file.close()
 
 print('bye!')
